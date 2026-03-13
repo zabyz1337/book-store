4c484e1eea769184617ac557d301a7524905cd66
IKolmykov <zabyz1337@gmail.com>
2026-03-13 11:49:47 +0100

    implement BOOK_REMOVE

--- src/store/store.js
+++ src/store/store.js
@@ -16,7 +16,7 @@ function libraryReducer(state = initialState, action) {
       const { title, author, year } = action.payload;
 
       const newBook = {
-        id: Date.now() + Math.random(),
+        id: Date.now() + Math.floor(Math.random() * 1000),
         title,
         author,
         year,
@@ -26,6 +26,27 @@ function libraryReducer(state = initialState, action) {
       return {
         ...state,
         books: [...state.books, newBook],
+        lastUpdated: new Date().toISOString(),
+      };
+    }
+
+    case BOOK_REMOVE: {
+      const bookToRemove = state.books.find(
+        (book) => book.id === action.payload,
+      );
+
+      if (!bookToRemove) {
+        return state;
+      }
+
+      if (bookToRemove.isAvailable === false) {
+        console.log("Нельзя удалить книгу, потому что она сейчас выдана");
+        return state;
+      }
+
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
         lastUpdated: new Date().toISOString(),
       };
     }
