f7e3ee2a7e140e8910d8eb78ed29893a01fba7c5
IKolmykov <zabyz1337@gmail.com>
2026-03-13 11:13:04 +0100

    add book action types

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,10 @@
 import { createStore } from "redux";
 
+export const BOOK_ADD = "BOOK_ADD";
+export const BOOK_REMOVE = "BOOK_REMOVE";
+export const BOOK_UPDATE_INFO = "BOOK_UPDATE_INFO";
+export const BOOK_TOGGLE_AVAILABILITY = "BOOK_TOGGLE_AVAILABILITY";
+
 const initialState = {
   books: [],
   lastUpdated: null,
